062d387bbb48e5f644e0d07d511771756f30a9e3
JC_onLine <jc.gillot@gmail.com>
2020-08-23 10:20:31 +0200

    feat(DB): WaitingRoom, InGame, WinnerList.:0.5

--- olr_api/models.py
+++ olr_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class WaitingRoom(models.Model):
+    name = models.CharField(default='No name', max_length=20)
+    in_comming_dt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class InGame(models.Model):
+    player1_name = models.CharField(default='', max_length=20)
+    player2_name = models.CharField(default='', max_length=20)
+
+    def __str__(self):
+        return f"{self.player1_name} {self.player1_name}"
+
+
+class WinnerList(models.Model):
+    name = models.CharField(default='', max_length=20)
+
+    def __str__(self):
+        return f"{self.name}"
