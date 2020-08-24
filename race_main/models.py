151455d94f09c50cd4da9d53fb140308027a46ab
JC_onLine <jc.gillot@gmail.com>
2020-08-24 22:43:04 +0200

    feat(status): Display rudimentary home page.:2

--- race_main/models.py
+++ race_main/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 
+class InGame(models.Model):
+    name = models.CharField(default='', max_length=20)
+    # player2_name = models.CharField(default='', max_length=20)
+    # player3_name = models.CharField(default='', max_length=20, blank=True)
+    # player4_name = models.CharField(default='', max_length=20, blank=True)
+
+    def __str__(self):
+        return f"{self.name} "
+
+
 class WaitingRoom(models.Model):
     name = models.CharField(default='No name', max_length=20)
     in_comming_dt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name}"
-
-
-class InGame(models.Model):
-    player1_name = models.CharField(default='', max_length=20)
-    player2_name = models.CharField(default='', max_length=20)
-    player3_name = models.CharField(default='', max_length=20)
-    player4_name = models.CharField(default='', max_length=20)
-
-    def __str__(self):
-        return f"{self.player1_name} {self.player1_name}"
 
 
 class WinnerList(models.Model):
--- race_main/views.py
+++ race_main/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .models import WaitingRoom, InGame, WinnerList
 
+
 def test_view(request):
     data = {
         'status': False,
@@ -14,16 +15,18 @@ def test_view(request):
     }
     return JsonResponse(data)
 
+
 def status_display(request):
-    waiting_room = WaitingRoom.objects.get()
-    in_game = InGame.objects.get()
-    winner_list = WinnerList.objects.get()
+    in_game = InGame.objects.all()
+    waiting_room = WaitingRoom.objects.all()
+    winner_list = WinnerList.objects.all()
     context = {
-        'waiting_room': waiting_room,
+        'status': True,
         'in_game': in_game,
+        'waiting_room': waiting_room,
         'winner_list': winner_list
     }
     return render(request,
-                  'reporting/template/activity_template_details.html',
+                  'home.html',
                   context)
 
